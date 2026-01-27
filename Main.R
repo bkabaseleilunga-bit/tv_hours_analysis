89fd927a1d8c568ea1b1f860b5bcff15056ecde3
bkabaseleilunga-bit <b.kabaseleilunga2023@gmail.com>
2026-01-27 20:12:11 +0100

    filter to those 30 and above

--- Main.R
+++ Main.R
@@ -3,9 +3,11 @@ library(here)
 
 tv_hours_table <- 
   gss_cat %>% 
+  filter (age >= 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours = 
               mean (tvhours, 
                     na.rm = T))
 
-write_csv(tv_hours_table, here("TV_hours_by_Marital.csv"))
+write_csv(tv_hours_table, 
+          here("TV_hours_by_Marital.csv"))
